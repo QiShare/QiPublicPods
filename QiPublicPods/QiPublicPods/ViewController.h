1ca67bf215faa6752be98abe40527cb0f27e7405
liusiqi <i-liusiqi1@360.cn>
2018-12-17 10:58:25 +0800

    new
    new

---
+++ QiPublicPods/QiPublicPods/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QiPublicPods
+//
+//  Created by liusiqi on 2018/12/11.
+//  Copyright © 2018 QiShare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QiPublicPods/QiPublicPods/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  QiPublicPods
+//
+//  Created by liusiqi on 2018/12/11.
+//  Copyright © 2018 QiShare. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    
+    [super viewDidLoad];
+    
+    NSString *str1 = @"1";
+    NSLog(@"%p",str1);
+    
+    str1 = @"2";
+    NSLog(@"%p",str1);
+}
+
+
+@end
