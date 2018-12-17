78dfbb86b92cd4dc398c2c53c08b801765832e1c
liusiqi <i-liusiqi1@360.cn>
2018-12-17 15:58:03 +0800

    update data

---
+++ QiPublicPods/QiPublicPods/Classes/QiPublicManager.h
@@ -0,0 +1,17 @@
+//
+//  QiPublicManager.h
+//  QiPublicPods
+//
+//  Created by liusiqi on 2018/12/17.
+//  Copyright © 2018 QiShare. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface QiPublicManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ QiPublicPods/QiPublicPods/Classes/QiPublicManager.m
@@ -0,0 +1,13 @@
+//
+//  QiPublicManager.m
+//  QiPublicPods
+//
+//  Created by liusiqi on 2018/12/17.
+//  Copyright © 2018 QiShare. All rights reserved.
+//
+
+#import "QiPublicManager.h"
+
+@implementation QiPublicManager
+
+@end
